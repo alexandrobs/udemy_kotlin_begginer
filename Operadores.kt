4c624dab66e9549028a2cf6559339efcc90522c2
Alexandro B S <jornalsinal@gmail.com>
2021-11-09 23:57:54 -0300

    Add files via upload

--- Operadores.kt
+++ Operadores.kt
@@ -2,8 +2,8 @@ class Operadores {
 }
 
 //sobre when
-fun operacao(a: Int, b: Int, operation: String): Int{
-    when (operation){
+fun operacao(a: Int, b: Int, operation: String): Int {
+    when (operation) {
         "Soma" -> {
             return a + b
         }
@@ -20,31 +20,31 @@ fun operacao(a: Int, b: Int, operation: String): Int{
     }
 }
 
-fun main(){
+fun main() {
     //tabela verdade com operadores
     //conjuncao && e
     //para retornar true precisam as duas premissas serem verdadeiras
     val a: Int = 1
     val b: Int = 2
 
-    if(a > 1 && b > 1){
+    if (a > 1 && b > 1) {
         println("true")
-    }else{
+    } else {
         println("false")
     }
 
     //disjuncao || ou
     //para retornar true basta que uma das premissas sejam verdadeiras
-    if(a > 1 || b > 1){
+    if (a > 1 || b > 1) {
         println("true")
-    }else{
+    } else {
         println("false")
     }
 
     var bool: Boolean = false
     //negacao ! not
     //para negar uma premissa
-    if(!bool){
+    if (!bool) {
         println(a)
     }
 
@@ -80,5 +80,73 @@ fun main(){
             println("Nada")
         }
     }
+
+    //for
+    val nome: String = "Alexandro Bernardino da Silva"
+
+    for (c in nome) {
+        print("$c ")
+    }
+
+    //usando for com Range e uso de step
+    for (i in 1..100 step 2) {
+        if (i < 2) {
+            println()
+        }
+        if (i != 80) {
+            print("$i ")
+        }
+    }
+
+    //usando o for com downTo
+    for (i in 100 downTo 0) {
+        println()
+        print("$i ")
+    }
+
+    //usando o while, o break que pode ser usado em while e for
+    //o while executa somente se a condição for verdadeira
+    var count: Int = 0
+
+    while (count < 100) {
+        println()
+        print(count)
+        count++
+        if (count == 50) {
+            break
+        }
+    }
+
+    //usando o do while, diferente do while aqui o do executa mesmo se a condição do while for falsa,
+    //mas executa apenas uma vez o que tem dentro do do
+
+    var countDoWhile: Int = 0
+    do {
+        println()
+        println(countDoWhile)
+        countDoWhile++
+    }   while (countDoWhile < 100)
+
+    //Testando a Teste.java aqui dentro
+    println()
+    //testando o operador ternario do java
+    //Teste().ternario()
+
+    //testando o NullSafe do kotlin
+    //Isso dispara um NullPointerException
+    //Teste().meuNome()
+    //NullSafe em kotlin
+    var str: String? = null
+    //Elvis operator para acessar propriedades que podem ser nulas
+    println(str?.length)
+    //ou
+//    if (str != null){
+//        println(str.length)
+//    }
+    //ou
+    //ou esse operador !! ele diz ao kotlin pra retornar o que tiver que retornar
+    // assumindo voce o erro que dispare isso
+    //println(str!!.length)
+
 }
 
