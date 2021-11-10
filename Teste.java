f5afdc85d12272017a38e1fa7eeebf50e40e9e68
Alexandro B S <jornalsinal@gmail.com>
2021-11-09 23:59:37 -0300

    Add files via upload

--- Teste.java
+++ Teste.java
@@ -1,20 +1,21 @@
 public class Teste {
 
     public void ternario(){
-        int valor = 10;
+        int valor = 1;
         //com operador ternario
-        String str = valor == 10 ? "Sim" : "Não";
-
+        String str = valor == 10 ? "Sim" : "Nao";
+        System.out.println(str);
         //sem operador ternario
-        String a;
-        if (valor == 10){
-            a = "Sim";
-        } else {
-            a = "Não";
-        }
+//        String a;
+//        if (valor == 10){
+//            a = "Sim";
+//        } else {
+//            a = "Não";
+//        }
     }
 
-    public static void main(String[] args) {
-        System.out.println("Olá mundo");
+    public void meuNome(){
+        String str = null;
+        str.length();
     }
 }
